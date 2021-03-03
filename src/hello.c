76f6ae42c7877e346e4f8e4752cef53b20a4902f
GNUOverYooouu <69485115+DamieFC@users.noreply.github.com>
2021-03-03 14:04:45 -0800

    Create hello.c
    Meh. Simple. Easy. Fast.

---
+++ src/hello.c
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ * This file, hello.c, is licensed under the Unlicese. See the file "LICENSE"  *
+ * for details.                                                                *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Says hello and exits.                                                       *
+ *******************************************************************************/
+
+#include <stdio.h>
+
+int main(){
+	printf("Hello!");
+	return 0
+}
